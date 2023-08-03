e400545ad64e8a3050fb1b1e55afb4c21ee07f12
Juener <juener@gmail.com>
2023-08-03 21:09:20 +0100

    last changes not approved

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    // disable: process.env.NODE_ENV === "development" ,
+  },
+  reactStrictMode: true,
+});
